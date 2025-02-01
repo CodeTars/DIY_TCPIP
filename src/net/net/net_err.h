a2761174e5682dcdba84300fecc910b95a1bdb0f
codetars <imtars@qq.com>
2025-02-01 21:01:14 +0800

    feat: 添加环回接口

--- src/net/net/net_err.h
+++ src/net/net/net_err.h
@@ -9,6 +9,7 @@ typedef enum _net_err_t {
     NET_ERR_MEM = -4,
     NET_ERR_SIZE = -5,
     NET_ERR_PARAM = -6,
+    NET_ERR_NONE = -7,
 } net_err_t;
 
 #endif
--- src/net/src/netif.c
+++ src/net/src/netif.c
@@ -19,7 +19,7 @@ net_err_t netif_init(void) {
     return NET_ERR_OK;
 }
 
-netif_t *netif_open(const char *dev_name) {
+netif_t *netif_open(const char *dev_name, const netif_ops_t *driver, void *driver_data) {
     netif_t *netif = (netif_t *)mblock_alloc(&netif_mblock, -1);
     if (!netif) {
         dbg_error(DBG_NETIF, "no netif");
@@ -35,9 +35,12 @@ netif_t *netif_open(const char *dev_name) {
 
     strncpy(netif->name, dev_name, NETIF_NAME_SIZE);
     netif->name[NETIF_NAME_SIZE - 1] = '\0';
+    netif->ops = driver;
+    netif->ops_data = driver_data;
 
     net_err_t err = fixq_init(&netif->in_q, netif->in_q_buf, NETIF_INQ_SIZE, NLOCKER_THREAD);
     if (err < 0) {
+        mblock_free(&netif_mblock, netif);
         dbg_error(DBG_NETIF, "netif in_q init error, err: %d", err);
         return (netif_t *)0;
     }
@@ -45,12 +48,32 @@ netif_t *netif_open(const char *dev_name) {
     err = fixq_init(&netif->out_q, netif->out_q_buf, NETIF_OUTQ_SIZE, NLOCKER_THREAD);
     if (err < 0) {
         fixq_destroy(&netif->in_q);
+        mblock_free(&netif_mblock, netif);
         dbg_error(DBG_NETIF, "netif in_q init error, err: %d", err);
         return (netif_t *)0;
     }
 
+    err = netif->ops->open(netif, driver_data);
+    if (err < 0) {
+        dbg_error(DBG_NETIF, "netif ops open error");
+        goto free_return;
+    }
     netif->state = NETIF_OPENED;
-    nlist_insert_last(&netif_list, &netif->node);
 
+    if (netif->type == NETIF_TYPE_NONE) {
+        dbg_error(DBG_NETIF, "netif type unknown");
+        goto free_return;
+    }
+
+    nlist_insert_last(&netif_list, &netif->node);
     return netif;
+
+free_return:
+    if (netif->state == NETIF_OPENED) {
+        netif->ops->close(netif);
+    }
+    fixq_destroy(&netif->in_q);
+    fixq_destroy(&netif->out_q);
+    mblock_free(&netif_mblock, netif);
+    return (netif_t *)0;
 }
